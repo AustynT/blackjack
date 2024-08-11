3657af999016c7dd644c948a5feb7dae6caeaf7c
AustynT <AustynTravis9@gmail.com>
2024-08-11 16:18:15 -0600

    added starter project code.

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ['./*.html'], // Specify where Tailwind should look for class usage
+    theme: {
+      extend: {}, // Extend Tailwind's default theme here
+    },
+    plugins: [],
+  };
